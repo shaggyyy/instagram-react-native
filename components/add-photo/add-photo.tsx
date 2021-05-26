7cd6e3ca734fc1938ba52f170026a50c8e44a80c
sagar.nigam <sagar.nigam@windmill.ch>
2021-05-26 23:50:11 +0530

    camera and taking picture functionality added

--- components/add-photo/add-photo.tsx
+++ components/add-photo/add-photo.tsx
@@ -1,10 +1,77 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
 
 export const addPhoto = () => {
+    const [hasPermission, setHasPermission] = useState(false);
+    const [camera, setCamera] = useState(null)
+    const [image, setImage] = useState('')
+    const [type, setType] = useState(Camera.Constants.Type.back);
+
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+
+    if (hasPermission === false) {
+        return <Text>No access to camera</Text>;
+    }
+
+    const takePicture = async () => {
+        if(camera) {
+            const data = await camera.takePictureAsync(null);
+            setImage(data.uri);
+        }
+    }
+
     return (
-        <View>
-            <Text>Add Photo</Text>
+        <View style={styles.pageContainer}>
+            <View style={styles.cameraContainer}>
+                <Camera ref={ref => setCamera(ref)} style={styles.fixedRatio} type={type}  ratio={'1:1'} />
+            </View>
+
+            <Button
+                title='Flip Image'
+                onPress={() => {
+                    setType(
+                        type === Camera.Constants.Type.back
+                            ? Camera.Constants.Type.front
+                            : Camera.Constants.Type.back
+                    );
+                }}>
+            </Button>
+
+            <Button 
+                title='take Picture'
+                onPress={() => takePicture()}
+            />.
+            { image && <Image source={{uri: image}}  style={styles.clickedImage}/> }
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    pageContainer: {
+        flex: 1,
+        flexDirection: 'column',
+    },
+    camera: {
+        flex: 1,
+    },
+    cameraContainer: {
+        flex: 1,
+    },
+    fixedRatio: {
+        flex: 1,
+        aspectRatio: 1,
+    },
+    clickedImage: {
+        flex: 1,
+    }
+});
