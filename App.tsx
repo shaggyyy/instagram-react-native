c62b8d3419634ef6f1d4e518c91a9586ee34650e
Sagar Nigam <sagarnigam07@gmail.com>
2021-06-17 10:07:46 +0530

    Develop (#11)
    * image picking functionality added
    
    * upload of image functionality added
    
    * saving the whole post
    
    * getting posts data from firestore
    
    * photos shown on profile page of user
    
    * user search functionality added
    
    * profile getting updated with respective uid
    
    * follow buton and functionality added
    
    * unfollow functionality added
    
    * pull changes
    
    * follow unfollow done
    
    * users issue solved
    
    * firebase config removed
    
    Co-authored-by: sagar.nigam <sagar.nigam@windmill.ch>

--- App.tsx
+++ App.tsx
@@ -19,20 +19,6 @@ import { saveImage } from './components/save-image/save-image';
 
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCiNshI_QxsVLHEmdODOnCWifo1WW_deZc",
-  authDomain: "instgram-clone-react-native.firebaseapp.com",
-  projectId: "instgram-clone-react-native",
-  storageBucket: "instgram-clone-react-native.appspot.com",
-  messagingSenderId: "725116981205",
-  appId: "1:725116981205:web:67b0867bbb3f3e16b1f75c",
-  measurementId: "G-BBQ86DN0CT"
-};
-
-if (firebase.apps.length === 0) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 const Stack = createStackNavigator();
 
 export default function App() {
